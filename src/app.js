e30aefe23abd1d1e0f3dae587eacd268fde566df
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-04-02 21:56:56 +0000

    feat: Random Card Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//TODO Create a general .card class that applies the general styles to each card.
+//TODO CSS class for each suit: .spade, .club, .heart & .diamond.
+//! ♦ ♥ ♠ ♣
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const RandomCard = () => {
+  const RandomCardSuit = () => {
+    const CardSuits = ["♦", "♥", "♠", "♣"];
+    const suits = document.querySelectorAll(".suits");
+
+    const RandomSuit = Math.floor(Math.random() * CardSuits.length);
+
+    CardSuits[RandomSuit] == "♦" || CardSuits[RandomSuit] == "♥"
+      ? suits.class.add("diamond")
+      : null;
+
+    return CardSuits[RandomSuit];
+  };
+
+  const RandomCardNumber = () => {
+    const CardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+    const RandomMainNumber = Math.floor(Math.random() * CardNumbers.length);
+
+    return CardNumbers[RandomMainNumber];
+  };
+
+  document.querySelector(".card-number").innerHTML = RandomCardNumber();
+};
+
+const SetTimer = () => window.setInterval(RandomCard, 6000);
+
+window.onload = () => {
+  RandomCard();
+  SetTimer();
 };

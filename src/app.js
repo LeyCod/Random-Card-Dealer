4a9995cdebdf3592f5fc749005683927f6c2a421
Emmanuel Leyan <emmanuel.leyan@gmail.com>
2022-04-03 16:22:22 +0000

    eatx: Suit color changing and functional button

--- src/app.js
+++ src/app.js
@@ -3,35 +3,51 @@ import "bootstrap";
 import "./style.css";
 
 //TODO Create a general .card class that applies the general styles to each card.
-//TODO CSS class for each suit: .spade, .club, .heart & .diamond.
 //! ♦ ♥ ♠ ♣
 
-const RandomCard = () => {
-  const RandomCardSuit = () => {
-    const CardSuits = ["♦", "♥", "♠", "♣"];
-    const suits = document.querySelectorAll(".suits");
-
-    const RandomSuit = Math.floor(Math.random() * CardSuits.length);
-
-    CardSuits[RandomSuit] == "♦" || CardSuits[RandomSuit] == "♥"
-      ? suits.class.add("diamond")
-      : null;
-
-    return CardSuits[RandomSuit];
-  };
-
-  const RandomCardNumber = () => {
-    const CardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-
-    const RandomMainNumber = Math.floor(Math.random() * CardNumbers.length);
-
-    return CardNumbers[RandomMainNumber];
-  };
-
-  document.querySelector(".card-number").innerHTML = RandomCardNumber();
+/* Card Suits */
+const CardSuits = {
+  "♦": "diamond",
+  "♥": "hearth",
+  "♠": "spade",
+  "♣": "club"
 };
 
-const SetTimer = () => window.setInterval(RandomCard, 6000);
+/* Card Numbers */
+const CardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+/* DOM Elements */
+const RandomMainNumber = document.querySelector(".card-number");
+const RandomCardButton = document.querySelector("#random-btn");
+const TopSuit = document.querySelector(".card-suit-top");
+const BottomSuit = document.querySelector(".card-suit-bottom");
+const ColorSuits = document.querySelectorAll(".suits");
+
+/* Functions */
+const RandomCard = () => {
+  const RandomSuit = Object.keys(CardSuits)[Math.floor(Math.random() * 4)]; //CardSuit Key obtained.
+  const SuitClass = CardSuits[RandomSuit]; //CardSuit Value obtained.
+
+  TopSuit.innerHTML = RandomSuit;
+  BottomSuit.innerHTML = RandomSuit;
+
+  RandomMainNumber.innerHTML =
+    CardNumbers[Math.floor(Math.random() * CardNumbers.length)]; //Print a random number from CardNumbers
+
+  if (SuitClass === "diamond" || SuitClass === "hearth") {
+    TopSuit.style.color = "red";
+    BottomSuit.style.color = "red";
+  } else {
+    TopSuit.style.color = "black";
+    BottomSuit.style.color = "black";
+  }
+};
+
+RandomCardButton.addEventListener("click", () => {
+  RandomCard();
+}); //Generate a random card clicking the button
+
+const SetTimer = () => window.setInterval(RandomCard, 10000); //timmer set every 10 seg.
 
 window.onload = () => {
   RandomCard();
